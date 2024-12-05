5d332159d4a17179ea14d7ed5f18fbf886d3d7b3
omkr27 <omkargawas46@gmail.com>
2024-12-05 16:49:12 +0530

    removed unnecesary parenthesis

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function deleteItemById(item, productId) {
 }
 
 //Endpoint 3
-app.get('/cart/delete, (req, res) => {
+app.get('/cart/delete', (req, res) => {
   let productId = parseInt(req.query.productId);
   let cartItems = cart.filter((item) => deleteItemById(item, productId));
   res.json({ cartItems: cartItems });
